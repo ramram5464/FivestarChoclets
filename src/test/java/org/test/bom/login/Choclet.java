0e2a0233338577cb0c56ed78dbf543d3c50d493e
ramram5464 <ramram62220@gmail.com>
2022-08-27 10:01:16 +0530

    Good Sweets

--- src/test/java/org/test/bom/login/Choclet.java
+++ src/test/java/org/test/bom/login/Choclet.java
@@ -1,5 +1,16 @@
 package org.test.bom.login;
 
 public class Choclet {
+	private void sweet1() {
+	System.out.println("Apple");
+}
+	private void sweet2() {
+		System.out.println("orange");
+}
+	private void sweet3() {
+System.out.println("grape");
+
+	}
+	
 
 }
